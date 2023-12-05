e1ef2f55235870f4a73025232a9d38585d7a09db
cssk7745 <130124558+cssk7745@users.noreply.github.com>
2023-12-06 01:41:20 +0530

    Create PrimeChecker.cpp

---
+++ PrimeChecker.cpp
@@ -0,0 +1,17 @@
+bool isPrime(int n)
+{
+	// Write your code here.
+	int cnt =0;
+	for(int i=1;i*i<=n;i++) 
+	{
+		if(n%i==0) {
+			cnt++;
+			if((n/i)!=i) cnt++;
+		}
+
+		
+	}
+	if(cnt==2) return true;
+	else return false;
+
+}
